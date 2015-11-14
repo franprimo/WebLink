87f9765f9d6204cf65241543a17a7a5bfdbbbcd2
FranPrimo <frapriru@gmail.com>
2015-11-14 16:35:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/franprimo/weblink/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.franprimo.weblink;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText textoEntrada = (EditText) findViewById(R.id.editText);
+        Button irBtn = (Button) findViewById(R.id.button);
+        final WebView wv = (WebView) findViewById(R.id.webView);
+
+        irBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = textoEntrada.getText().toString();
+                wv.loadUrl(url);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
